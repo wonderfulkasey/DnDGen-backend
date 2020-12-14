43187ef73c2648214d1032257114bcf6c8133555
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-14 15:30:02 -0500

    added attributes to weapon serializer

--- app/serializers/weapon_serializer.rb
+++ app/serializers/weapon_serializer.rb
@@ -1,6 +1,6 @@
 class WeaponSerializer
   include FastJsonapi::ObjectSerializer
-  attributes
-  #attributes :name, :weapon_category, :weapon_range, :url, :equipment_id
+  #attributes
+  attributes :name, :weapon_category, :weapon_range, :url, :equipment_id
   belongs_to :equipment 
 end
