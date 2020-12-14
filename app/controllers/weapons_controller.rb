1f26ee1535244ee2d8bd17c077a6ebce24ed52d3
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-14 14:44:25 -0500

    added serializer and base controller

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,2 +1,23 @@
 class WeaponsController < ApplicationController
+
+    def index 
+        @weapons = Weapon.all 
+        
+        render json: @weapons
+    end 
+
+    def create
+        @weapon.new(weapon_params)
+        if @weapon.save
+            render json: @weapon, status: :accepted
+        else  
+            render json: { errors: @weapon.errors.full_messages }, status: :unprocessible_entity
+        end 
+    end
+
+private 
+
+    def weapon_params
+        params.permit(:name, :weapon_category, :weapon_range, :url, :equipment_id)
+    end 
 end
