c03d4846fdcdbaabd758ad00dbb8928ad8b4d20f
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-14 12:12:48 -0500

    added equipment model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+spooky = Equipment.create(name: "Spooky")
+scratchy = Equipment.create(name: "Scratchy")
+splendid = Equipment.create(name: "splendid")
