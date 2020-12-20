809e61f23a86b4a9ffa5e614539fccd0ae73a51d
wonderfulkasey <kaseyarnold15@gmail.com>
2020-12-20 11:54:52 -0500

    serializer belongs to

--- app/serializers/weapon_serializer.rb
+++ app/serializers/weapon_serializer.rb
@@ -2,5 +2,5 @@ class WeaponSerializer
   include FastJsonapi::ObjectSerializer
   #attributes
   attributes :name, :weapon_category, :weapon_range, :url, :equipment_id, :equipment
-  #belongs_to :equipment 
+  belongs_to :equipment 
 end
