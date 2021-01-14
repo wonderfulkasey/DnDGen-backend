c2f3055600a08879c61128eb29b1e3ac2acbc35f
wonderfulkasey <kaseyarnold15@gmail.com>
2021-01-14 12:11:41 -0500

    added the index route and spell check

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -2,8 +2,9 @@ class EquipmentController < ApplicationController
 
 def index
     equipments = Equipment.all
-    render json: EquipmentSerializer.new
+    render json: EquipmentSerializer.new(equipments)
 end
+
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :weapons, only: [:index, :create]
-  resources :equipments, only: [:index]
+  resources :equipment, only: [:index, :create]
   resources :ideas, only: [:index, :create]
   
  
